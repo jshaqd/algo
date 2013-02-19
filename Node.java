65528fbcfecf155015c0ebfc6eb0d026d54adca1
Angela Qian <Angela.qiandan@gmail.com>
2013-02-18 22:51:44 -0600

    Deque

--- Node.java
+++ Node.java
@@ -3,4 +3,10 @@ public class Node<Item> {
 	public Item Value;
 	public Node<Item> Next;
 	public Node<Item> Previous;
+	
+	public Node(Item item) {
+		Value = item;
+		Next = null;
+		Previous = null;
+	}
 }
