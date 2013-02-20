a58c8c1c3d842c7c699b913c85a58fbe87ab9ae1
Angela Qian <Angela.qiandan@gmail.com>
2013-02-19 20:54:47 -0600

    submit

--- Deque.java
+++ Deque.java
@@ -2,133 +2,134 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
-	
-	private class Node<Item> {
-		public Item Value;
-		public Node<Item> Next;
-		public Node<Item> Previous;
-		
-		public Node(Item item) {
-			Value = item;
-			Next = null;
-			Previous = null;
-		}
-	}
-	private Node<Item> head;
-	private Node<Item> tail;
-	private int size;
+ private class Node<Item> {
+  private Item value;
+  private Node<Item> next;
+  private Node<Item> previous;
+  
+  public Node(Item item) {
+   value = item;
+   next = null;
+   previous = null;
+  }
+ }
+ private Node<Item> head;
+ private Node<Item> tail;
+ private int size;
 
-	public Deque() {
-		head = new Node(null);
-		tail = new Node(null);
-		head.Next = tail;
-		tail.Previous = head;
-	}
+ public Deque() {
+  head = new Node(null);
+  tail = new Node(null);
+  head.next = tail;
+  tail.previous = head;
+ }
 
-	public boolean isEmpty() {
-		return size == 0;
-	}
+ public boolean isEmpty() {
+  return size == 0;
+ }
 
-	public int size() {
-		return size;
-	}
+ public int size() {
+  return size;
+ }
 
-	public void addFirst(Item item) {
-		if (item == null)
-			throw new NullPointerException("client attempts to add a null item");
-		Node<Item> node = new Node(item);
-		node.Next = head.Next;
-		head.Next.Previous = node;
-		head.Next = node;
-		node.Previous = head;
-		size++;
-	}
+ public void addFirst(Item item) {
+  if (item == null)
+   throw new NullPointerException("client attempts to add a null item");
+  Node<Item> node = new Node(item);
+  node.next = head.next;
+  head.next.previous = node;
+  head.next = node;
+  node.previous = head;
+  size++;
+ }
 
-	public void addLast(Item item) {
-		if (item == null)
-			throw new NullPointerException("client attempts to add a null item");
-		Node<Item> node = new Node(item);
-		tail.Previous.Next = node;
-		node.Next = tail;
-		node.Previous = tail.Previous;
-		tail.Previous = node;
-		size++;
-	}
+ public void addLast(Item item) {
+  if (item == null)
+   throw new NullPointerException(
+       "client attempts to add a null item");
+  Node<Item> node = new Node(item);
+  tail.previous.next = node;
+  node.next = tail;
+  node.previous = tail.previous;
+  tail.previous = node;
+  size++;
+ }
 
-	public Item removeFirst() {
-		if (isEmpty())
-			throw new NoSuchElementException(
-					"client attempts to remove an item from an empty deque");
-		Node<Item> node = head.Next;
-		head.Next = node.Next;
-		node.Next.Previous = head;
-		Item item = node.Value;
-		node = null;
-		size--;
-		return item;
-	}
+ public Item removeFirst() {
+  if (isEmpty())
+   throw new NoSuchElementException(
+     "client attempts to remove an item from an empty deque");
+  Node<Item> node = head.next;
+  head.next = node.next;
+  node.next.previous = head;
+  Item item = node.value;
+  node = null;
+  size--;
+  return item;
+ }
 
-	public Item removeLast() {
-		if (isEmpty())
-			throw new NoSuchElementException(
-					"client attempts to remove an item from an empty deque");
-		Node<Item> node = tail.Previous;
-		head.Previous = node.Previous;
-		node.Previous.Next = tail;
-		Item item = node.Value;
-		node = null;
-		size--;
-		return item;
-	}
+ public Item removeLast() {
+  if (isEmpty())
+   throw new NoSuchElementException(
+     "client attempts to remove an item from an empty deque");
+  Node<Item> node = tail.previous;
+  tail.previous = node.previous;
+  node.previous.next = tail;
+  Item item = node.value;
+  node = null;
+  size--;
+  return item;
+ }
 
-	public Iterator<Item> iterator() {
-		return new DequeIterator();
-	}
+ public Iterator<Item> iterator() {
+  return new DequeIterator();
+ }
 
-	private class DequeIterator<Item> implements Iterator<Item> {
-		private Node<Item> current = (Node<Item>) head;
+ private class DequeIterator<Item> implements Iterator<Item> {
+  private Node<Item> current = (Node<Item>) head;
 
-		public boolean hasNext() {
-			return current.Next != tail;
-		}
+   public boolean hasNext() {
+   return current.next != tail;
+  }
 
-		public void remove() {
-			throw new UnsupportedOperationException(
-					"client calls the remove() method in the iterator");
-		}
+  public void remove() {
+   throw new UnsupportedOperationException(
+     "client calls the remove() method in the iterator");
+  }
 
-		public Item next() {			
-			if (hasNext()) {
-				Item item = current.Next.Value;
-				current = current.Next;
-				return item;
-			} else
-				throw new NoSuchElementException(
-						"there are no more items to return in the iterator");
-		}
-	}
+  public Item next() {   
+   if (hasNext()) {
+    Item item = current.next.value;
+    current = current.next;
+    return item;
+   } else
+    throw new NoSuchElementException(
+      "there are no more items to return in the iterator");
+  }
+ }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+ /**
+  * @param args
+  */
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
 
-		Deque<Integer> deque = new Deque<Integer>();
-		deque.addFirst(5);
-		deque.addFirst(6);
-		StdOut.println(deque.size());
-		deque.removeLast();
-		StdOut.println(deque.size());
-		deque.removeLast();
-	
-		//Iterator<Integer> iterator = deque.iterator();
-		//iterator.remove();		
+  Deque<Integer> deque = new Deque<Integer>();
+  deque.addFirst(5);
+  deque.addFirst(6);
+  StdOut.println(deque.size());
+  int item = deque.removeLast();
+  StdOut.println(item);
+  item = deque.removeLast();
+  StdOut.println(item);
+ 
+  //Iterator<Integer> iterator = deque.iterator();
+  //iterator.remove();  
 
-		//for (Integer i : deque) {
-			//StdOut.println(i);
-		//}
+  //for (Integer i : deque) {
+   //StdOut.println(i);
+  //}
 
-	}
+ }
 
 }
--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -2,144 +2,145 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-	private int size = 4, count;
-	private Item[] array;
-	private int first, last;
+    private int size = 4, count;
+ private Item[] array;
+ private int first, last;
 
-	public RandomizedQueue() {
-		array = (Item[]) new Object[size];
-	}
+ public RandomizedQueue() {
+  array = (Item[]) new Object[size];
+ }
 
-	private void resize(Item[] old, int capacity) {
-		Item[] newArray = (Item[]) new Object[capacity];
-		for (int i = 0; i < count; i++) {
-			newArray[i] = old[getArrayIndex(first + i, size)];
-		}
+ private void resize(Item[] old, int capacity) {
+  Item[] newArray = (Item[]) new Object[capacity];
+  for (int i = 0; i < count; i++) {
+   newArray[i] = old[getArrayIndex(first + i, size)];
+  }
 
-		size = capacity;
-		array = newArray;
-		first = 0;
-		last = first + count;
-	}
+  size = capacity;
+  array = newArray;
+  first = 0;
+  last = first + count;
+ }
 
-	private int getArrayIndex(int i, int size) {
-		return i % size;
-	}
+ private int getArrayIndex(int i, int sz) {
+  return i % sz;
+ }
 
-	public boolean isEmpty() {
-		return count == 0;
-	}
+ public boolean isEmpty() {
+  return count == 0;
+ }
 
-	public int size() {
-		return count;
-	}
+ public int size() {
+  return count;
+ }
 
-	public void enqueue(Item item) {
-		if (item == null)
-			throw new NullPointerException(
-					"the client attempts to add a null item");
-		array[last] = item;
-		last++;
-		last = getArrayIndex(last, size);
-		count++;
+ public void enqueue(Item item) {
+  if (item == null)
+   throw new NullPointerException(
+     "the client attempts to add a null item");
+  array[last] = item;
+  last++;
+  last = getArrayIndex(last, size);
+  count++;
 
-		if (count == size) {
-			resize(array, size * 2);
-		}
-	}
+  if (count == size) {
+   resize(array, size * 2);
+  }
+ }
 
-	public Item dequeue() {
-		if (count == 0)
-			throw new NoSuchElementException(
-					"the client attempts to deque an item from an empty randomized queue");
+ public Item dequeue() {
+  if (count == 0)
+   throw new NoSuchElementException(
+     "the client attempts to deque an item from an empty randomized queue");
 
-		int index = getRandomIndex(count);
-		int pos = getArrayIndex(first + index, size);
-		Item item = (Item) array[pos];
-		array[pos] = array[first];
-		array[first] = null;
-		first++;
-		first = first % size;
+  int index = getRandomIndex(count);
+  int pos = getArrayIndex(first + index, size);
+  Item item = (Item) array[pos];
+  array[pos] = array[first];
+  array[first] = null;
+  first++;
+  first = first % size;
 
-		count--;
+  count--;
 
-		if (count >= 4 && count < size / 4) {
-			resize(array, size / 4);
-		}
+  if (count >= 4 && count < size / 4) {
+   resize(array, size / 4);
+  }
 
-		return item;
-	}
+  return item;
+ }
 
-	public Item sample() {
-		if (count == 0)
-			throw new NoSuchElementException(
-					"the client attempts to sample an item from an empty randomized queue");
+ public Item sample() {
+  if (count == 0)
+   throw new NoSuchElementException(
+     "the client attempts to sample an item from an empty randomized queue");
 
-		Iterator<Item> iterator = this.iterator();
-		Item item = iterator.next();
-		return item;
-	}
+  Iterator<Item> iterator = this.iterator();
+  Item item = iterator.next();
+  return item;
+ }
 
-	private int getRandomIndex(int number) {
-		return StdRandom.uniform(number);
-	}
+ private int getRandomIndex(int number) {
+  return StdRandom.uniform(number);
+ }
 
-	public Iterator<Item> iterator() {
-		return new RandomizedQueueIterator();
-	}
+ public Iterator<Item> iterator() {
+  return new RandomizedQueueIterator();
+ }
 
-	private class RandomizedQueueIterator<Item> implements Iterator<Item> {
-		private int current = first;
-		private Item[] randomizedArray;
+ private class RandomizedQueueIterator<Item> implements Iterator<Item> {
+  private int current = first;
+  private Item[] randomizedArray;
 
-		public RandomizedQueueIterator() {
-			// StdOut.println("iterator constructor");
-			randomizedArray = (Item[]) array.clone();
-			for (int i = 0; i < count; i++) {
-				int random = StdRandom.uniform(i + 1);
-				int pos = getArrayIndex(first + i, size);
-				int posRandom = getArrayIndex(first + random, size);
-				
-				Item item = randomizedArray[pos];
-				randomizedArray[pos] = randomizedArray[posRandom];
-				randomizedArray[posRandom] = item;
-			}
-		}
+  public RandomizedQueueIterator() {
+   // StdOut.println("iterator constructor");
+   randomizedArray = (Item[]) array.clone();
+   for (int i = 0; i < count; i++) {
+    int random = StdRandom.uniform(i + 1);
+    int pos = getArrayIndex(first + i, size);
+    int posRandom = getArrayIndex(first + random, size);
+    
+    Item item = randomizedArray[pos];
+    randomizedArray[pos] = randomizedArray[posRandom];
+    randomizedArray[posRandom] = item;
+   }
+  }
 
-		public boolean hasNext() {
-			return (first > last && (current >= first || current < last) || (first < last && current < last));
-		}
+  public boolean hasNext() {
+   return (first > last && (current >= first || current < last)
+               || (first < last && current < last));
+  }
 
-		public void remove() {
-			throw new UnsupportedOperationException(
-					"client calls the remove() method in the iterator");
-		}
+  public void remove() {
+   throw new UnsupportedOperationException(
+     "client calls the remove() method in the iterator");
+  }
 
-		public Item next() {
-			if (hasNext()) {
-				Item item = (Item) randomizedArray[current];
-				current++;
-				current = getArrayIndex(current, size);
-				return item;
-			} else
-				throw new NoSuchElementException(
-						"there are no more items to return in the iterator");
-		}
-	}
+  public Item next() {
+   if (hasNext()) {
+    Item item = (Item) randomizedArray[current];
+    current++;
+    current = getArrayIndex(current, size);
+    return item;
+   } else
+    throw new NoSuchElementException(
+      "there are no more items to return in the iterator");
+  }
+ }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		RandomizedQueue<Integer> queue = new RandomizedQueue<Integer>();
+ /**
+  * @param args
+  */
+ public static void main(String[] args) {
+  RandomizedQueue<Integer> queue = new RandomizedQueue<Integer>();
 
-		for (int i = 1; i <= 20; i++) {
-			queue.enqueue(i);
-		}
+  for (int i = 1; i <= 20; i++) {
+   queue.enqueue(i);
+  }
 
-		for (int i = 1; i <= 19; i++) {
-			int item = queue.dequeue();
-			queue.enqueue(item + 1);
-		}
-	}
+  for (int i = 1; i <= 19; i++) {
+   int item = queue.dequeue();
+   queue.enqueue(item + 1);
+  }
+ }
 }
